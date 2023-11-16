7f9527df6a04163ed76f3f59084741c087e3f589
esdraslov <edvan.marcelino56@gmail.com>
2023-11-15 21:53:11 -0300

    added arguments

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import argparse
 
-
+parser = argparse.ArgumentParser()
+parser.add_argument("action")
+parser.add_argument("target", help="the file to be target, no every action need it", required=False)
+parser.add_argument("extra", required=False, help="some extra info, no every action need it")
+args = parser.parse_args()
